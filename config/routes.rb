602da2b23ec04c743b85221def79e18e7004fde1
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2022-12-28 11:37:44 +0000

    ルーティング作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,47 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # 会員用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 会員用
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
+
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'costomers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  # 管理者用
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

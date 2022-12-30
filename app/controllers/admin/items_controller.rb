a958dd49d64308b1fe16eb7d7e13a7d56407da1f
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2022-12-30 15:26:14 +0000

    配送先登録画面作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path
   end
 
   def show
@@ -15,6 +19,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
   end
 
 end

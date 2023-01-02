38d07422a1285d67c7ccbf5ff117fbf2f38f4ad5
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2023-01-02 12:04:32 +0000

    配送先一覧画面の修正、配送先編集機能作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,7 +5,7 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
-    @address = Address.find()
+    @address = Address.find(params[:id])
   end
 
   def create
@@ -16,6 +16,9 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy

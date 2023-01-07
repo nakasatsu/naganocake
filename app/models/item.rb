7d050eb028668d8479be47863ae00d66c64edba5
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2023-01-07 07:12:23 +0000

    admin/itemsのshow,editに画像関連機能を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,13 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details
   belongs_to :genre
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage_500.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [250, 200]).processed
+  end
+
 end

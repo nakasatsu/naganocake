e2454cec8c8b31f10a07251e4d5a2ac88df4a268
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2023-01-08 03:05:21 +0000

    会員が退会している場合の処理を記述（作業中）

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,12 +2,30 @@
 
 class Public::SessionsController < Devise::SessionsController
 
+  before_action :customer_state, only: [:create]
+  
   def after_sign_in_path_for(resource)
     root_path
   end
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+  
+  protected
+  
+  def customer_state
+    # 【処理１】入力されたemailからアカウントを１件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    # アカウントを取得できなかった場合、メソッドを終了する
+    return if !@customer
+    # 【処理２】取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      # 【処理３】１と２の処理がtrueなら、そのアカウントのis_deletedカラムを確認し、
+      # trueの場合、サインアップ画面に遷移
+      # falseの場合、createアクションを実行させる
+      
+    end
   end
   # before_action :configure_sign_in_params, only: [:create]
 

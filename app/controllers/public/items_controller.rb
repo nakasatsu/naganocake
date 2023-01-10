65c44b851d86e50812d92113878d09621d63dc26
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2023-01-10 10:32:58 +0000

    public/itemsのindex,showを作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
+    @item = Item.all
     @items = Item.page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,8 @@ class Item < ApplicationRecord
     image.variant(resize_to_limit: [250, 200]).processed
   end
 
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
 end

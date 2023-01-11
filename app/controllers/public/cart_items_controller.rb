01ae17d06c0095a0039c1c70266ba813d2bf5e0e
EC2 Default User <ec2-user@ip-172-31-16-95.ap-northeast-1.compute.internal>
2023-01-11 11:18:28 +0000

    カートに入れる機能、カート内商品表示機能を実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+    @total = 0
   end
 
   def update
@@ -12,6 +14,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 
 end

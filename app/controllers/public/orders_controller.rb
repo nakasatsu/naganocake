332f10dcecc896e926f4919364d019e80f24c5e0
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-01-19 06:36:13 +0000

    注文情報確認画面を作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,17 +7,22 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_items = current_customer.cart_items
     @total = 0
-    @order = Order.new(order_params)
-    if params[:order][:select_address] == 0
+    if params[:order][:select_address] == "0"
+      @order = Order.new(order_params)
+      @order.shipping_cost = 800
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_address] == 1
+    elsif params[:order][:select_address] == "1"
+      @order = Order.new(order_params)
+      @order.shipping_cost = 800
       @address = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-    else
+    elsif params[:order][:select_address] == "2"
+      @order = Order.new(order_params)
+      @order.shipping_cost = 800
     end
   end
 

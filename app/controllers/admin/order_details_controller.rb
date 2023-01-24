d43dae9ee09f5b216d7fe6590fa6d3d9778d2af0
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-01-24 09:40:26 +0000

    ステータスの自動更新機能実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,28 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details
+    is_updated = true
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order_detail.order.id)
+    @order.update(order_status: 2) if @order_detail.making_status_i18n == "製作中"
+      # if @order_detail.making_status_i18n == "製作中"
+        ### @order.update(order_status: 2)
+        # @order.order_status = Order.order_statuses.key(2)
+        # @order.save
+      # end
+      @order_details.each do |order_detail|
+        is_updated = false if order_detail.making_status_i18n != "製作完了"
+        # if order_detail.making_status_i18n != "製作完了"
+          # is_update = false
+        # end
+      end
+      @order.update(order_status: 3) if is_updated
+      # if is_update == true
+        # @order.order_status = Order.order_statuses.key(3)
+        # @order.save
+      # end
+    redirect_to admin_order_path(@order.id)
   end
   
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,7 +7,15 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    # 注文ステータスの更新後、製作ステータスの更新を行う
+    if @order.order_status == "confirmed"
+      @order.order_details.each do |order_detail|
+        order_detail.making_status = OrderDetail.making_statuses.key(1)
+        order_detail.save
+      end
+    end
     redirect_to admin_order_path(@order.id)
+    
   end
   
   private
